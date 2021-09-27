7b79eaf0a1daa528d8c19ee23db88fc620745cd4
niluh1701 <niluh1701@users.noreply.github.com>
2021-09-27 18:49:38 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,20 @@
+
+
+animals = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+
+print(animals)
+
+print(animals[0])
+print(animals[1])
+print(animals[2])
+print(animals[3])
+print(animals[4])
+print(animals[5])
+print(animals[6])
+print(animals[7])
+
+#ani2.sort()
+
+print(animals)
+
+print(animals.index("Cat"))
